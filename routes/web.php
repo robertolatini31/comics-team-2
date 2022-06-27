0f9ba2bf778603306bd089c4f8259f3fdf63f470
robertolatini31 <robertolatini31@gmail.com>
2022-06-27 16:50:51 +0200

    add routes

--- routes/web.php
+++ routes/web.php
@@ -48,4 +48,14 @@ Route::get('/books/{id}', function ($id) {
 })->name('books_show');
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+
+Route::middleware('auth')->namespace('Admin')->name('admin.')->prefix('admin')->group(function(){
+    Route::get('/', 'HomeController@index')->name('home');
+    Route::resource('comics', 'ComicController')->parameters([
+        'comics' => 'comic:slug',
+    ]);
+    Route::resource('series', 'SerieController')->parameters([
+        'series' => 'serie:slug',
+    ])->except(['show', 'create', 'edit']);
+});
+
