ad8281991af77523ef3f858a62c76aa56dd1dc9d
robertolatini31 <robertolatini31@gmail.com>
2022-06-27 16:16:08 +0200

    add login in header

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+@extends('_layouts._app')
 
 @section('content')
     <main id="site_main">
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -4,6 +4,40 @@
             <div class="d-flex align-items-center justify-content-end p-1 text-white">
                     <span class="text-uppercase me-5">dc power visa&reg;</span>
                     <span class="text-uppercase">additional dc sites <i class="fa-solid fa-caret-down ms-1"></i></span>
+
+                    <div class="px-2">
+                    <ul class="navbar-nav ml-auto d-flex flex-row  gap-1">
+                        <!-- Authentication Links -->
+                        @guest
+                            <li class="nav-item mr-3">
+                                <a class="text-white" href="{{ route('login') }}">{{ __('Login') }}</a>
+                            </li>
+                            @if (Route::has('register'))
+                                <li class="nav-item mr-3">
+                                    <a class="text-white" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                </li>
+                            @endif
+                        @else
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ Auth::user()->name }}
+                                </a>
+
+                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </li>
+                        @endguest
+                    </ul>
+                    </div>
             </div>
         </div>
     </div>
